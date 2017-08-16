ca8357127d6fcc9538c0099e3ee656e61f3a87b4
Paul Touma <paul_touma@brown.edu>
2017-08-16 12:15:03 -0400

    initial commit

--- IdenticonImageCompression/main.c
+++ IdenticonImageCompression/main.c
@@ -6,10 +6,31 @@
 //  Copyright © 2017 Paul Touma. All rights reserved.
 //
 
+#include <math.h>
 #include <stdio.h>
 
+const int identicon_pixel_length = 3;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+   
+    int possible_identicons = pow(2,identicon_pixel_length);
+    
+    for(int i = 0; i < possible_identicons; i++) {
+        generate_identicons(i);
+    }
+    
 }
+
+int (*generate_identicons(int identicon_id))[identicon_pixel_length][identicon_pixel_length] {
+
+    for (int i = 0; i < identicon_pixel_length; i++) {
+        
+        for (int j = 0; j < identicon_pixel_length; j++) {
+            
+        }
+    }
+}
+
+
+
+

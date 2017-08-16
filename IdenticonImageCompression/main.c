0fb7831d894e061568d96040059fab141dd0311f
Paul Touma <paul_touma@brown.edu>
2017-08-15 23:20:59 -0400

    Initial Commit

---
+++ IdenticonImageCompression/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  IdenticonImageCompression
+//
+//  Created by Paul Touma on 8/15/17.
+//  Copyright © 2017 Paul Touma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
